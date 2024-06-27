0fec47762ba04456b8a95e8facdb0bd283233d48
machadinhos <pbmachado2002@gmail.com>
2024-06-27 23:55:41 +0100

    fix footer

--- src/components/global-components/Footer.tsx
+++ src/components/global-components/Footer.tsx
@@ -5,6 +5,7 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import WhatsappIcon from "@mui/icons-material/WhatsApp";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import { primaryColor, secondaryColor } from "@/styles/globals";
+import Link from "next/link";
 
 interface Props {
   lang: LangEnum;
@@ -12,24 +13,51 @@ interface Props {
 
 export default function Footer({ lang }: Props) {
   return (
-    <footer style={{ backgroundColor: secondaryColor, color: primaryColor, padding: "1rem", position: "relative" }}>
+    <footer
+      style={{
+        backgroundColor: secondaryColor,
+        color: primaryColor,
+        padding: "1rem",
+        position: "relative",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Link href={`/${lang}/`}>
+          <Image alt={"logo"} src={"/logo.png"} width={150} height={150} />
+        </Link>
+      </Box>
       <Grid container sx={{ justifyContent: "space-between" }}>
         <List sx={{ display: "flex", flexDirection: "column", justifyContent: "space-evenly", alignItems: "center" }}>
-          <ListItem>
-            <a href={`/${lang}/`}>{selectLang(lang, "home")}</a>
-          </ListItem>
-          <ListItem>
-            <a href={`/${lang}/services`}>{selectLang(lang, "services")}</a>
-          </ListItem>
-          <ListItem>
-            <a href={`/${lang}/gallery`}>{selectLang(lang, "gallery")}</a>
-          </ListItem>
-          <ListItem>
-            <a href={`/${lang}/contacts`}>{selectLang(lang, "contacts")}</a>
-          </ListItem>
-          <ListItem>
-            <a href={`/${lang}/faq`}>FAQ</a>
-          </ListItem>
+          <Link href={`/${lang}/`}>
+            <ListItem>{selectLang(lang, "home")}</ListItem>
+          </Link>
+          <Link href={`/${lang}/services`}>
+            <ListItem>{selectLang(lang, "services")}</ListItem>
+          </Link>
+          <Link href={`/${lang}/catering`}>
+            <ListItem>Catering</ListItem>
+          </Link>
+          <Link href={`/${lang}/gallery`}>
+            <ListItem>{selectLang(lang, "gallery")}</ListItem>
+          </Link>
+          <Link href={`/${lang}/contacts`}>
+            <ListItem>{selectLang(lang, "contacts")}</ListItem>
+          </Link>
+          <Link href={`/${lang}/faq`}>
+            <ListItem>FAQ</ListItem>
+          </Link>
         </List>
         <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
           <Typography sx={{ color: primaryColor }}>{selectLang(lang, "footer-follow")}</Typography>
@@ -46,21 +74,6 @@ export default function Footer({ lang }: Props) {
           </List>
         </Box>
       </Grid>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-        }}
-      >
-        <Image alt={"logo"} src={"/logo.png"} width={150} height={150} />
-      </Box>
       <Typography sx={{ textAlign: "center", color: primaryColor }}>
         {selectLang(lang, "footer-rights-reserved")}
       </Typography>
