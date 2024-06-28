35743cc68b9665b46cea234851e73e026eefe718
manuelfesantos@gmail.com <manuelfesantos@gmail.com>
2024-06-29 00:12:15 +0100

    mudar os sub links em mobile

--- src/components/global-components/Main.tsx
+++ src/components/global-components/Main.tsx
@@ -4,11 +4,87 @@ import WhatsappIcon from "@mui/icons-material/WhatsApp";
 import { Box, Container } from "@mui/material";
 import { LangEnum } from "@i18n/lang-selector";
 import { HomeCarousel } from "@/components/page-components/Home";
+import { primaryColor, secondaryColor } from "@/styles/globals";
+import Image from "next/image";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 
 interface Props {
   children: ReactNode | ReactNode[];
   isHome?: boolean;
   lang: LangEnum;
+}
+
+function MobileBottomButtons({ lang }: { lang: LangEnum }) {
+  return (
+    <Box
+      display={{ xs: "block", sm: "none" }}
+      position={"fixed"}
+      bottom={0}
+      left={0}
+      width={"100vw"}
+      height={"5rem"}
+      sx={{ backgroundColor: primaryColor }}
+    >
+      <Box width={"100%"} height={"100%"} display={"flex"} justifyContent={"center"} alignItems={"center"}>
+        <Link href={`/${lang}/`}>
+          <Image alt={"logo"} src={"/logo.png"} width={75} height={60} />
+        </Link>
+      </Box>
+      <Box position={"absolute"} bottom={"1rem"} left={"1rem"}>
+        <Link
+          target={"_blank"}
+          href={"https://api.whatsapp.com/send/?phone=351915581629&text&type=phone_number&app_absent=0"}
+        >
+          <WhatsappIcon sx={{ fontSize: "3rem" }} color={"success"} />
+        </Link>
+      </Box>
+      <Box position={"absolute"} right={"1rem"} bottom={"1rem"}>
+        <Link href={"#header"}>
+          <ArrowUpwardIcon sx={{ fontSize: "3rem", color: "white" }} />
+        </Link>
+      </Box>
+    </Box>
+  );
+}
+
+function DesktopBottomButtons({ lang }: { lang: LangEnum }) {
+  return (
+    <Box display={{ xs: "none", sm: "block" }} position={"fixed"} bottom={0} left={0} width={"100vw"} height={"5rem"}>
+      <Box position={"absolute"} bottom={"1rem"} left={"1rem"}>
+        <Link
+          target={"_blank"}
+          href={"https://api.whatsapp.com/send/?phone=351915581629&text&type=phone_number&app_absent=0"}
+        >
+          <WhatsappIcon sx={{ fontSize: "4rem" }} color={"success"} />
+        </Link>
+      </Box>
+      <Box
+        position={"absolute"}
+        right={"1rem"}
+        bottom={"1rem"}
+        width={"4rem"}
+        height={"4rem"}
+        border={`3px solid ${primaryColor}`}
+        borderRadius={"50%"}
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Link href={"#header"}>
+          <ArrowUpwardIcon sx={{ fontSize: "4rem", color: secondaryColor }} />
+        </Link>
+      </Box>
+    </Box>
+  );
+}
+
+function BottomButtons({ lang }: { lang: LangEnum }) {
+  return (
+    <>
+      <MobileBottomButtons lang={lang} />
+      <DesktopBottomButtons lang={lang} />
+    </>
+  );
 }
 
 export default function Main({ children, isHome, lang }: Props) {
@@ -34,15 +110,8 @@ export default function Main({ children, isHome, lang }: Props) {
         }}
       >
         {children}
-        <Box sx={{ position: "fixed", bottom: "2rem", left: "0.8rem" }}>
-          <Link
-            target={"_blank"}
-            href={"https://api.whatsapp.com/send/?phone=351915581629&text&type=phone_number&app_absent=0"}
-          >
-            <WhatsappIcon sx={{ fontSize: "4rem" }} fontSize={"large"} color={"success"} />
-          </Link>
-        </Box>
       </Container>
+      <BottomButtons lang={lang} />
     </main>
   );
 }
