8b67064d6e7564db873569f8c64b9ad6d872dfa1
machadinhos <pbmachado2002@gmail.com>
2024-06-29 03:12:09 +0100

    fix gallery for mobile

--- src/components/page-components/Gallery.tsx
+++ src/components/page-components/Gallery.tsx
@@ -10,23 +10,63 @@ export default function Gallery({ lang }: Props) {
     <>
       <Typography variant={"h1"}>{selectLang(lang, "gallery")}</Typography>
       <section>
-        <Typography variant="h3">{selectLang(lang, "gallery-title-1")}</Typography>
+        <Typography
+          sx={{
+            fontWeight: "bold",
+            fontSize: { xs: "1.75rem", sm: "2.25rem", md: "2.75rem", lg: "3.25rem" },
+          }}
+          variant="h3"
+        >
+          {selectLang(lang, "gallery-title-1")}
+        </Typography>
         <Typography variant="body1">{selectLang(lang, "gallery-text-1")}</Typography>
       </section>
       <section>
-        <Typography variant="h3">{selectLang(lang, "gallery-title-2")}</Typography>
+        <Typography
+          sx={{
+            fontWeight: "bold",
+            fontSize: { xs: "1.75rem", sm: "2.25rem", md: "2.75rem", lg: "3.25rem" },
+          }}
+          variant="h3"
+        >
+          {selectLang(lang, "gallery-title-2")}
+        </Typography>
         <Typography variant="body1">{selectLang(lang, "gallery-text-2")}</Typography>
       </section>
       <section>
-        <Typography variant="h3">{selectLang(lang, "gallery-title-3")}</Typography>
+        <Typography
+          sx={{
+            fontWeight: "bold",
+            fontSize: { xs: "1.75rem", sm: "2.25rem", md: "2.75rem", lg: "3.25rem" },
+          }}
+          variant="h3"
+        >
+          {selectLang(lang, "gallery-title-3")}
+        </Typography>
         <Typography variant="body1">{selectLang(lang, "gallery-text-3")}</Typography>
       </section>
       <section>
-        <Typography variant="h3">{selectLang(lang, "gallery-title-4")}</Typography>
+        <Typography
+          sx={{
+            fontWeight: "bold",
+            fontSize: { xs: "1.75rem", sm: "2.25rem", md: "2.75rem", lg: "3.25rem" },
+          }}
+          variant="h3"
+        >
+          {selectLang(lang, "gallery-title-4")}
+        </Typography>
         <Typography variant="body1">{selectLang(lang, "gallery-text-4")}</Typography>
       </section>
       <section>
-        <Typography variant="h3">{selectLang(lang, "gallery-title-5")}</Typography>
+        <Typography
+          sx={{
+            fontWeight: "bold",
+            fontSize: { xs: "1.75rem", sm: "2.25rem", md: "2.75rem", lg: "3.25rem" },
+          }}
+          variant="h3"
+        >
+          {selectLang(lang, "gallery-title-5")}
+        </Typography>
         <Typography variant="body1">{selectLang(lang, "gallery-text-5")}</Typography>
       </section>
     </>
