bbd98b87b9472d30925c8fd6736939edabdedbe6
manuelfesantos@gmail.com <manuelfesantos@gmail.com>
2024-06-29 01:33:26 +0100

    fix font loading delay

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <link rel="preload" href="/fonts/elipses-caps.otf" as="font" type={"font/otf"} crossOrigin={"anonymous"} />
       <Head />
       <body>
         <Main />
