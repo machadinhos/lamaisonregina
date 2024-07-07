7a00d3311b8a0322ad7f58b34dbb73e077140d1e
manuelfesantos@gmail.com <manuelfesantos@gmail.com>
2024-07-07 05:33:53 +0100

    fix mobile bottom buttons

--- src/components/shared-components/BottomButtons.tsx
+++ src/components/shared-components/BottomButtons.tsx
@@ -50,7 +50,7 @@ export function MobileBottomButtons({ lang }: { lang: LangEnum }) {
       width={"100%"}
       height={"5rem"}
       sx={{ backgroundColor: primaryColor }}
-      zIndex={10}
+      zIndex={40}
       id={"mobile-bottom-buttons"}
     >
       <Box width={"100%"} height={"100%"} display={"flex"} justifyContent={"center"} alignItems={"center"}>
