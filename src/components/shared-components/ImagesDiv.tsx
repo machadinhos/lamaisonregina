52a3e0c95ee8d1c3da2a8e0a213563e6b1d3e145
machadinhos <pbmachado2002@gmail.com>
2024-07-12 17:20:21 +0100

    make ImagesDiv collumn on mobile

--- src/components/shared-components/ImagesDiv.tsx
+++ src/components/shared-components/ImagesDiv.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function ImagesDiv({ imagesSrc }: { imagesSrc: [string, string, string] }) {
   const imageBoxStyle = {
-    width: `${(1 / 3) * 100}%`,
+    width: { xs: "100%", md: `${(1 / 3) * 100}%` },
     transition: "width 0.4s ease-in-out",
     "&:hover": {
       width: "100%",
@@ -12,7 +12,14 @@ export default function ImagesDiv({ imagesSrc }: { imagesSrc: [string, string, s
   };
 
   return (
-    <Box display={"flex"} justifyContent={"center"} alignItems={"center"} height={"25rem"} width={"100%"}>
+    <Box
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      height={{ xs: "75rem", md: "25rem" }}
+      width={"100%"}
+      flexDirection={{ xs: "column", md: "row" }}
+    >
       {imagesSrc.map((src, index) => (
         <Box key={index} position="relative" height={"100%"} sx={imageBoxStyle}>
           <Image src={src} alt={`image-${index}`} style={{ objectFit: "cover" }} fill />
