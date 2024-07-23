ccd708184881cbffa55bdf41132f99d55548a199
machadinhos <pbmachado2002@gmail.com>
2024-07-23 10:41:11 +0100

    make text italic when it's surrounded by _

--- src/components/shared-components/Typography/GenericPageText.tsx
+++ src/components/shared-components/Typography/GenericPageText.tsx
@@ -9,7 +9,35 @@ interface Props {
   sx?: SxProps<Theme>;
 }
 
+const processChild = (child: string, array: childrenType[]) => {
+  const splitChild = child.split("_");
+  if (splitChild.length === 1) {
+    array.push(child);
+    return;
+  }
+  for (let i = 0; i < splitChild.length; i++) {
+    if (splitChild[i] !== "") {
+      if (i % 2 === 0) {
+        array.push(splitChild[i]);
+      } else {
+        array.push(<i key={i}>{splitChild[i]}</i>);
+      }
+    }
+  }
+};
+
 export default function GenericPageText({ children, sx }: Props) {
+  const finalChildren: childrenType[] = [];
+  if (Array.isArray(children)) {
+    for (const child of children) {
+      if (typeof child === "string") {
+        processChild(child, finalChildren);
+      }
+    }
+  } else {
+    processChild(children, finalChildren);
+  }
+
   return (
     <Typography
       variant={"body1"}
@@ -22,7 +50,7 @@ export default function GenericPageText({ children, sx }: Props) {
         ...sx,
       }}
     >
-      {children}
+      {finalChildren}
     </Typography>
   );
 }
