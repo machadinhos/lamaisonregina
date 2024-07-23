7522780e9673855a75b6aca525508a30a22377ac
machadinhos <pbmachado2002@gmail.com>
2024-07-23 11:02:35 +0100

    change home welcome text

--- src/components/page-components/Home/HomeInfo.tsx
+++ src/components/page-components/Home/HomeInfo.tsx
@@ -2,22 +2,15 @@ import { homeLang, LangEnum } from "@i18n/lang-selector";
 import React from "react";
 import { Box } from "@mui/material";
 import GenericPageText from "@/components/shared-components/Typography/GenericPageText";
-import GenericPageMainTitle from "@/components/shared-components/Typography/GenericPageMainTitle";
-import GenericPageSubTitle from "@/components/shared-components/Typography/GenericPageSubTitle";
 import Image from "next/image";
+import GenericPageTitle from "@/components/shared-components/Typography/GenericPageTitle";
 
 export function HomeInfo({ lang }: { lang: LangEnum }) {
   return (
     <section>
       <Box pt={{ xs: "2rem", md: "0" }}>
         <Box mb={"4rem"}>
-          <GenericPageSubTitle sx={{ textAlign: "center", fontWeight: "200", fontSize: "2rem" }}>
-            {homeLang(lang, "home-welcome-1")}
-          </GenericPageSubTitle>
-          <GenericPageMainTitle>La Maison Regina</GenericPageMainTitle>
-          <GenericPageSubTitle sx={{ textAlign: "center", fontWeight: "200", fontSize: "2rem" }}>
-            {homeLang(lang, "home-welcome-2")}
-          </GenericPageSubTitle>
+          <GenericPageTitle>{homeLang(lang, "home-welcome")}</GenericPageTitle>
         </Box>
         <Box display={"flex"} flexDirection={{ xs: "column", md: "row" }}>
           <Box pr={{ md: "1.5rem" }} width={{ xs: "100%", md: `${(2 / 3) * 100}%` }}>
