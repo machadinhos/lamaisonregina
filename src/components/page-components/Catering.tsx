b6d5b0d78e5d2baf56c45128be0284ada934ae82
machadinhos <pbmachado2002@gmail.com>
2024-07-24 22:54:02 +0100

    change Catering page to use GenericPageTitle

--- src/components/page-components/Catering.tsx
+++ src/components/page-components/Catering.tsx
@@ -2,7 +2,7 @@ import { cateringLang, LangEnum } from "@i18n/lang-selector";
 import GenericPageText from "@/components/shared-components/Typography/GenericPageText";
 import CTA from "@/components/shared-components/CTA";
 import React from "react";
-import GenericPageSubTitle from "@/components/shared-components/Typography/GenericPageSubTitle";
+import GenericPageTitle from "@/components/shared-components/Typography/GenericPageTitle";
 
 interface Props {
   lang: LangEnum;
@@ -12,16 +12,16 @@ export default function Catering({ lang }: Props) {
   return (
     <>
       <section>
-        <GenericPageSubTitle>{cateringLang(lang, "catering-title-1")}</GenericPageSubTitle>
+        <GenericPageTitle>{cateringLang(lang, "catering-title-1")}</GenericPageTitle>
         <GenericPageText>{cateringLang(lang, "catering-text-1")}</GenericPageText>
         <GenericPageText>{cateringLang(lang, "catering-text-2")}</GenericPageText>
       </section>
       <section>
-        <GenericPageSubTitle>{cateringLang(lang, "catering-title-2")}</GenericPageSubTitle>
+        <GenericPageTitle>{cateringLang(lang, "catering-title-2")}</GenericPageTitle>
         <GenericPageText>{cateringLang(lang, "catering-text-3")}</GenericPageText>
       </section>
       <section>
-        <GenericPageSubTitle>{cateringLang(lang, "catering-title-3")}</GenericPageSubTitle>
+        <GenericPageTitle>{cateringLang(lang, "catering-title-3")}</GenericPageTitle>
         <GenericPageText>{cateringLang(lang, "catering-text-4")}</GenericPageText>
       </section>
       <CTA lang={lang} />
