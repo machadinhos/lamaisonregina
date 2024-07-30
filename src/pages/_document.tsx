3eee7df2d1645108e0018e2d7f0f1bbbd4968c9a
machadinhos <pbmachado2002@gmail.com>
2024-07-30 12:09:17 +0100

    refactor _document.tsx to stop using class

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,34 +1,28 @@
-import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from "next/document";
+import { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from "next/document";
 
-interface CustomDocumentProps extends DocumentInitialProps {
-  lang: string;
+function Document({ lang }: { lang: string }) {
+  return (
+    <Html lang={lang}>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
 
-class CustomDocument extends Document<CustomDocumentProps> {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    const lang = ctx.asPath?.split("/")[1] ?? "pt";
-    return { ...initialProps, lang };
-  }
+Document.getInitialProps = async (ctx: DocumentContext): Promise<DocumentInitialProps & { lang: string }> => {
+  const initialProps = await ctx.defaultGetInitialProps(ctx);
+  const lang = ctx.asPath?.split("/")[1] ?? "pt";
+  return { ...initialProps, lang };
+};
 
-  render() {
-    return (
-      <Html lang={this.props.lang}>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"anonymous"} />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
-
-export default CustomDocument;
+export default Document;
