cd2f5cace9c9b8e1ac031d350b88f4357da779e2
machadinhos <pbmachado2002@gmail.com>
2024-08-02 20:36:00 +0100

    add Google Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from "next/document";
 import { globalsLang, LangEnum } from "@i18n/lang-selector";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 function Document({ lang }: { lang: string }) {
   const langEnum = lang === "pt" ? LangEnum.PT : LangEnum.EN;
@@ -8,7 +9,6 @@ function Document({ lang }: { lang: string }) {
     <Html lang={lang}>
       <Head>
         <meta charSet="utf-8" />
-        <meta content="KK4vxFcOK2imtY2lB7dkMzc3-JBI9vwwfPmaABIprko" name="google-site-verification" />
         <meta content={globalsLang(langEnum, "meta-description")} name="description" />
         <meta content={globalsLang(langEnum, "meta-keywords")} name="keywords" />
         <meta content="index, follow" name="robots" />
@@ -18,6 +18,7 @@ function Document({ lang }: { lang: string }) {
         <Main />
         <NextScript />
       </body>
+      <GoogleAnalytics gaId="G-DDBD6FPCJV" />
     </Html>
   );
 }
