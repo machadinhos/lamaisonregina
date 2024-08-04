031849d944fa1df0fc9b56005289f400230f2aa3
machadinhos <pbmachado2002@gmail.com>
2024-08-04 18:51:35 +0100

    add IconButton to CTA

--- src/components/shared-components/CTA.tsx
+++ src/components/shared-components/CTA.tsx
@@ -1,5 +1,5 @@
 import { globalsLang, LangEnum } from "@i18n/lang-selector";
-import { Box, Button } from "@mui/material";
+import { Box, Button, IconButton } from "@mui/material";
 import Link from "next/link";
 
 import GenericPageText from "@/components/shared-components/Typography/GenericPageText";
@@ -10,23 +10,25 @@ export default function CTA({ lang }: { lang: LangEnum }) {
     <>
       <SectionContainer>
         <Box display={"flex"} justifyContent={"center"} marginTop={"3rem"}>
-          <Link href={`/${lang}/contacts/#header`} style={{ width: "max-content" }}>
-            <Button
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                textTransform: "none",
-                padding: "1rem",
-                pt: "0",
-                margin: 0,
-                borderRadius: 0,
-              }}
-              variant={"contained"}
-            >
-              <GenericPageText sx={{ color: "white" }}>{globalsLang(lang, "cta")}</GenericPageText>
-            </Button>
-          </Link>
+          <IconButton>
+            <Link href={`/${lang}/contacts/#header`} style={{ width: "max-content" }}>
+              <Button
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  textTransform: "none",
+                  padding: "1rem",
+                  pt: "0",
+                  margin: 0,
+                  borderRadius: 0,
+                }}
+                variant={"contained"}
+              >
+                <GenericPageText sx={{ color: "white" }}>{globalsLang(lang, "cta")}</GenericPageText>
+              </Button>
+            </Link>
+          </IconButton>
         </Box>
       </SectionContainer>
     </>
