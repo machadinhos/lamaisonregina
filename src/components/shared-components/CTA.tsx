82330271f5f2943c10ff6499ff5d121e4d0dc6b6
machadinhos <pbmachado2002@gmail.com>
2024-08-04 19:32:08 +0100

    prevent CTA button from turning blue when clicked

--- src/components/shared-components/CTA.tsx
+++ src/components/shared-components/CTA.tsx
@@ -1,32 +1,34 @@
 import { globalsLang, LangEnum } from "@i18n/lang-selector";
-import { Box, Button } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
 import Link from "next/link";
 
 import GenericPageText from "@/components/shared-components/Typography/GenericPageText";
 import SectionContainer from "@/components/shared-components/SectionContainer";
+import { primaryColor } from "@/styles/globals";
 
 export default function CTA({ lang }: { lang: LangEnum }) {
   return (
     <>
       <SectionContainer>
         <Box display={"flex"} justifyContent={"center"} marginTop={"3rem"}>
-          <Link href={`/${lang}/contacts/#header`}>
-            <Button
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                textTransform: "none",
-                padding: "1rem",
-                pt: "0",
-                margin: 0,
-                borderRadius: 0,
-              }}
-              variant={"contained"}
-            >
+          <IconButton
+            sx={{
+              backgroundColor: primaryColor,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              textTransform: "none",
+              padding: "1rem",
+              pt: "0",
+              margin: 0,
+              borderRadius: 0,
+              "&:hover": { backgroundColor: primaryColor },
+            }}
+          >
+            <Link href={`/${lang}/contacts/#header`}>
               <GenericPageText sx={{ color: "white" }}>{globalsLang(lang, "cta")}</GenericPageText>
-            </Button>
-          </Link>
+            </Link>
+          </IconButton>
         </Box>
       </SectionContainer>
     </>
