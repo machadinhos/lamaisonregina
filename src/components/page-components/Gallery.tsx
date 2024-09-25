82891e1592cd72a4cfc52eec2f9974f1ef25bfe3
machadinhos <pbmachado2002@gmail.com>
2024-09-26 00:13:11 +0100

    change salary carousels as Laura wants them

--- src/components/page-components/Gallery.tsx
+++ src/components/page-components/Gallery.tsx
@@ -8,12 +8,21 @@ import GenericPageText from "@/components/shared-components/Typography/GenericPa
 import CTA from "@/components/shared-components/CTA";
 import SlickCarousel from "@/components/shared-components/Carousel/SlickCarousel";
 import SectionContainer from "@/components/shared-components/SectionContainer";
+import useWindowWidth from "@/hooks/use-window-width";
+import ImageModalWrapper from "@/components/shared-components/ImageModal/ImageModalWrapper";
+import BaseImage from "@/components/shared-components/ImageModal/BaseImage";
 
 interface Props {
   lang: LangEnum;
 }
 
 export default function Gallery({ lang }: Props) {
+  const screenWidth = useWindowWidth();
+
+  if (screenWidth === null) return null;
+
+  const getMaxWidth = () => Math.min(500, screenWidth * 0.85);
+
   return (
     <>
       <SectionContainer>
@@ -27,7 +36,16 @@ export default function Gallery({ lang }: Props) {
         <Box display={"flex"} justifyContent={"center"} mb={"1rem"}>
           <GenericPageTitle>{galleryLang(lang, "gallery-title-2")}</GenericPageTitle>
         </Box>
-        <SlickCarousel images={imageSelect.gallery.carousel2} />
+        {/*<SlickC|arousel images={imageSelect.gallery.carousel2} />*/}
+        <ImageModalWrapper>
+          <Box display={"flex"} justifyContent={"center"} width={"100%"}>
+            <Box height={"350px"} sx={{ flexShrink: 0 }} width={`${getMaxWidth()}px`}>
+              <Box alignItems={"center"} display={"flex"} height={"100%"} justifyContent={"center"} width={"100%"}>
+                <BaseImage alt={imageSelect.gallery.carousel2.alt} src={imageSelect.gallery.carousel2.src} />
+              </Box>
+            </Box>
+          </Box>
+        </ImageModalWrapper>
         <GenericPageText>{galleryLang(lang, "gallery-text-2")}</GenericPageText>
       </SectionContainer>
       <SectionContainer>
