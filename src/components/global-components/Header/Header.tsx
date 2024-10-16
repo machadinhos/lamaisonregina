6c3268bc61f358500e966ca18787cd9a9236ce8e
machadinhos <pbmachado2002@gmail.com>
2024-10-17 00:59:08 +0100

    stop desktop header from appearing in mobile devices for a fraction of a second

--- src/components/global-components/Header/Header.tsx
+++ src/components/global-components/Header/Header.tsx
@@ -1,5 +1,7 @@
 import { LangEnum } from "@i18n/lang-selector";
 import { useMediaQuery } from "@mui/system";
+import { useEffect, useState } from "react";
+import { Box } from "@mui/material";
 
 import MobileHeader from "@/components/global-components/Header/MobileHeader";
 import DesktopHeader from "@/components/global-components/Header/DesktopHeader";
@@ -11,10 +13,17 @@ interface Props {
 
 function Header({ lang, isHome }: Props) {
   const isSmallScreen = useMediaQuery("(max-width:600px)");
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   return (
     <header id={"header"}>
-      {isSmallScreen ? <MobileHeader isHome={isHome} lang={lang} /> : <DesktopHeader isHome={isHome} lang={lang} />}
+      <Box sx={{ opacity: isMounted ? 1 : 0 }}>
+        {isSmallScreen ? <MobileHeader isHome={isHome} lang={lang} /> : <DesktopHeader isHome={isHome} lang={lang} />}
+      </Box>
     </header>
   );
 }
